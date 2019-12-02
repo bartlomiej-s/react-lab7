4d71e95b6523a98132c947ccca72f99df4699f97
Bartek <bartek77@live.com>
2019-12-03 00:01:07 +0100

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div>
-      Empty App
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      employees: [],
+      loaded: false
+    }
+  }
+
+  getData() {
+    let url= "http://localhost:3004/employees"
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        let employees = data.map((employee, index) => {
+          let k = employee.isActive
+          let col = "black"
+          if (!k) col = "red"
+          return (
+            <div key={index}>
+              <p style={{margin: "10px 0px 0px 100px", color: col}}>{employee.name} {employee.age}<br /></p>
+            </div>
+          )
+        })
+        this.setState({
+          employees: employees,
+          loaded: true
+        });
+      })
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    const AppVar = (
+     <div className="App">
+        {this.state.loaded && <h1 style={{margin: "20px 0px 20px 50px"}}>Employees</h1>}
+        {this.state.loaded ? this.state.employees : 'Loading...'}
+     </div>
+    )
+
+    return AppVar
+  }
+
 }
 
-export default App;
+export default App
