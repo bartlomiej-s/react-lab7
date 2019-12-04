77b1346da6ccad7a7b1490a5981bf94df17a4302
Bartek <bartek77@live.com>
2019-12-04 18:09:27 +0100

    Task 4

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ class App extends React.Component {
       loaded: false,
       age: 18,
       badphone: false,
-      bademail: false
+      bademail: false,
+      validationpassed: "no"
     }
     this.ageChange = this.ageChange.bind(this)
     this.submit = this.submit.bind(this)
+    this.validatePhone = this.validatePhone.bind(this)
+    this.validateEmail = this.validateEmail.bind(this)
   }
 
   getData() {
@@ -42,45 +45,101 @@ class App extends React.Component {
   }
 
   ageChange(evt) {
+    var age1 = this.state.age
+    var age2 = evt.target.value
+    if (age1<18 && age2>=18)
+    {
+      var emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      var emailinput = document.getElementsByName('parentphoneno')[0]
+      if(emailinput)
+      {
+        if(emailinput.value.match(emailregex))
+        {
+          this.setState({
+            bademail: false,
+            validationpassed: ""
+          })
+        }
+        else
+        {
+          this.setState({
+            bademail: true,
+            validationpassed: "no"
+          })
+        }
+      }
+    }
+    else if (age1>=18 && age2<18)
+    {
+      var phoneregex = /^\d{9}$/
+      var phoneinput = document.getElementsByName('email')[0]
+      if(phoneinput)
+      {
+        if(phoneinput.value.match(phoneregex))
+        {
+          this.setState({
+            badphone: false,
+            validationpassed: ""
+          })
+        }
+        else
+        {
+          this.setState({
+            badphone: true,
+            validationpassed: "no"
+          })
+        }
+      }
+    }
     this.setState({
-      age: evt.target.value,
-      bademail: false,
-      badphone: false
+      age: evt.target.value
     })
   }
 
   submit() {
-    var phoneregex = /^\d{9}$/
+    this.validateEmail()
+    this.validatePhone()
+  }
+
+  validateEmail() {
     var emailregex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    var phoneinput = document.getElementsByName('parentphoneno')[0]
     var emailinput = document.getElementsByName('email')[0]
-    if(phoneinput)
-    {
-      if(phoneinput.value.match(phoneregex))
-      {
-        this.setState({
-          badphone: false
-        })
-      }
-      else
-      {
-        this.setState({
-         badphone: true
-        })
-      }
-    }
     if(emailinput)
     {
       if(emailinput.value.match(emailregex))
       {
         this.setState({
-          bademail: false
+          bademail: false,
+          validationpassed: ""
         })
       }
       else
       {
        this.setState({
-          bademail: true
+          bademail: true,
+          validationpassed: "no"
+        })
+      }
+    }
+  }
+
+  validatePhone() {
+    var phoneregex = /^\d{9}$/
+    var phoneinput = document.getElementsByName('parentphoneno')[0]
+    if(phoneinput)
+    {
+      if(phoneinput.value.match(phoneregex))
+      {
+        this.setState({
+          badphone: false,
+          validationpassed: ""
+        })
+      }
+      else
+      {
+        this.setState({
+         badphone: true,
+         validationpassed: "no"
         })
       }
     }
@@ -96,7 +155,7 @@ class App extends React.Component {
           <input style={{margin: "0px 0px 40px 44px", width: "60%"}} type="text" name="parentname" />
           <br />
           <label style={{margin: "0px 0px 0px 10%"}}>Parent Phone No.</label>
-          <input style={{margin: "0px 0px 0px 13px", width: "60%"}} type="text" name="parentphoneno" />
+          <input style={{margin: "0px 0px 0px 13px", width: "60%"}} type="text" name="parentphoneno" onChange={this.validatePhone} />
           <p align="center">{this.state.badphone && "Wrong phone no."}</p>
           <br />
       </div>
@@ -110,7 +169,7 @@ class App extends React.Component {
           <input style={{margin: "0px 0px 40px 93px", width: "60%"}} type="text" name="name" />
           <br />
           <label style={{margin: "0px 0px 0px 10%"}}>Email</label>
-          <input style={{margin: "0px 0px 0px 98px", width: "60%"}} type="text" name="email" />
+          <input style={{margin: "0px 0px 0px 98px", width: "60%"}} type="text" name="email" onChange={this.validateEmail}/>
           <p align="center">{this.state.bademail && "Wrong email"}</p>
           <br />
       </div>
@@ -129,7 +188,7 @@ class App extends React.Component {
         <div className="Form">
           <form>
             {this.state.age<18 ? underageForm : normalForm}
-            <input style={{margin: "0px 50px 25px 0px", float: "right"}} onClick={this.submit} type="button" value="Submit" />
+            <input style={{margin: "0px 50px 25px 0px", float: "right"}} disabled={this.state.validationpassed} onClick={this.submit} type="button" value="Submit" />
           </form>
         </div>
       </div>
